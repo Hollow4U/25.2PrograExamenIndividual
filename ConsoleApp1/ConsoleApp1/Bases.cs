4e5844594d857b264d61f05d1bf1419e28d008a5
Hollow4U <nanishinde123@gmail.com>
2025-09-25 19:17:39 -0500

    Generate Resources in base - David Cardenas

---
+++ ConsoleApp1/ConsoleApp1/Bases.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Bases
+    {
+        int _resource;
+        int _hp;
+
+        internal Bases(int resource, int hp)
+        {
+            this._resource = resource;
+            this._hp = hp;
+        }
+
+        internal void RecolectResources()
+        {
+            _resource += 10;
+        }
+
+        internal void LoseHP(int _loseHP)
+        {
+            _hp -= _loseHP;
+
+            if (-_hp <= 0)
+            {
+                Console.WriteLine("End Game");
+            }
+        }
+    }
+}
